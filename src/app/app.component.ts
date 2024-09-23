0ed18839f4290c229b09d27d5ef5b2fe55910eaf
Mario Olivares <meolivares06@gmail.com>
2024-09-23 10:21:56 -0300

    Main Nav layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftnavComponent } from './leftnav/leftnav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeftnavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
